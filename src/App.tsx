d51f5ce98d12bbd0b601cb528da2c743fae773fd
Ethan Tal Jaffe <jaffe413@gmail.com>
2019-09-09 15:36:22 +0300

    Added Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css'; 
-
+import './App.css';
 
 const App = () => {
-  const canvasRef = React.useRef(null);
   return (
     <div>hello world</div>
   );
